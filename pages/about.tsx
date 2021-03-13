19d781597f4dc90eadf6730a213e24032be07bb4
Alberto <alberto.fragoso@conekta.com>
2021-03-13 16:00:02 -0500

    Add static about page route.

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import { Article } from "../components/Article";
+
+const About = () => (
+  <Article>
+    <h1>About this blog</h1>
+    <p>This is the best blog ever. 🙂</p>
+  </Article>
+);
+
+export default About;
